64abbe3e48b7fa2f5cba2f1af201504733b2bd64
christylernz <holochris_za@yahoo.co.uk>
2018-09-25 21:12:33 +1200

    Moved NFC read function to wrapper
    Moved the NFC read or NFC watch to the NFC Wrapper.
    
    Removed the calls to old NFC functions

--- js/webnfcwrapper.js
+++ js/webnfcwrapper.js
@@ -3,6 +3,16 @@ var NFCNotSupportedException = {
     "message": "Unable to detect NFC support."
 };
 
+var NFCURLUndefinedException = {
+    "name": "URLUndefinedException",
+    "message": "An NFC Message was found with an undefined or invalid URL."
+};
+
+var NFCMessageEmptyException = {
+    "name": "NFCMessageEmptyException",
+    "message": "An NFC Message was found with no valid content."
+};
+
 function writeNFC(url, typeOfRecord, record) {
     if ('nfc' in navigator) {
         navigator.nfc.push({
@@ -13,6 +23,31 @@ function writeNFC(url, typeOfRecord, record) {
             }]
         });
         return "Success";
+    } else {
+        throw NFCNotSupportedException;
+    }
+}
+
+function readNFC() {
+    if ('nfc' in navigator) {
+        return navigator.nfc.watch(function (message) {
+                if (typeof (message.url) != "undefined") {
+                    if (message.records[0].recordType === 'empty') {
+                        throw NFCMessageEmptyException;
+                    } else {
+                        return JSON.stringify(message);
+                    }
+                } else {
+                    throw NFCURLUndefinedException;
+                }
+            }, {
+                mode: "any"
+            })
+            .then(function () {
+                return "Added a watch."
+            }).catch(function (err) {
+                return err.name;
+            });
     } else {
         throw NFCNotSupportedException;
     }
